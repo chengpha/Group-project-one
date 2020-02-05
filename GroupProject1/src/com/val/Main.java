154d77ce4fb5616ff3e0a87cfea45f9b7bc6c6a3
valkalen <60166074+valkalen@users.noreply.github.com>
2020-02-05 09:25:44 -0600

    Update Main.java

--- GroupProject1/src/com/val/Main.java
+++ GroupProject1/src/com/val/Main.java
@@ -11,18 +11,21 @@ public class Main {
         Controller controller = new Controller();
 
         //read in all json shipment files from "input" directory and move them to "processed" directory after processing;
-        List<Warehouse> warehouseList = controller.processJsonInputFile(MessageFormat.format("{0}/input/test.json", System.getProperty("user.dir")),
+        String msg = controller.processJsonInputFile(MessageFormat.format("{0}/input/test.json", System.getProperty("user.dir")),
                 MessageFormat.format("{0}/processed", System.getProperty("user.dir")));
 
+        System.out.println(msg);
+
         //get the first warehouse in the array and export its shipments to output directory
-        if(warehouseList == null)
+        if(controller.getWarehouseList().isEmpty())
             return;
 
-        Warehouse warehouse = warehouseList.get(0);
+        Warehouse warehouse = controller.getWarehouseList().get(0);
         warehouse.exportAllShipmentsToFile(MessageFormat.format( "{0}/output",System.getProperty("user.dir")));
 
         //show the list of all shipments for each warehouse
-        controller.printAllWarehousesWithShipments();
+        System.out.println("\n\nShow the list of all shipments for each warehouse: ");
+        System.out.println(controller.printAllWarehousesWithShipments());
     }
 }
 
